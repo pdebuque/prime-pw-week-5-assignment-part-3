d37e63b10a06ad34af02a45a4db4352638715e76
Paolo Debuque <pdebuque@gmail.com>
2022-09-30 09:52:51 -0500

    completed first task

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,13 @@
 console.log('***** Music Collection *****')
+
+const collection=[array];
+
+function addToCollection(title, artist, yearPublished) {
+    const newRecord = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished
+    }
+    collection.push(newRecord);
+    return newRecord
+}
