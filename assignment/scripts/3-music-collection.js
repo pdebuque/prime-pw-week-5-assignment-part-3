9492131f702ee8ec6a13760179502f2bd06e124d
Paolo Debuque <pdebuque@gmail.com>
2022-09-30 09:57:49 -0500

    completed second task

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,6 +1,6 @@
 console.log('***** Music Collection *****')
 
-const collection=[array];
+const collection=[];
 
 function addToCollection(title, artist, yearPublished) {
     const newRecord = {
@@ -11,3 +11,12 @@ function addToCollection(title, artist, yearPublished) {
     collection.push(newRecord);
     return newRecord
 }
+
+addToCollection('Sgt. Pepper\'s Lonely Hearts Club Band', 'The Beatles', 1967);
+addToCollection('Freudian','Daniel Caesar', 2017);
+addToCollection('Songs in the Key of Life','Stevie Wonder',1976);
+addToCollection('Carrie and Lowell', 'Sufjan Stevens',2015);
+addToCollection('The White Album', 'The Beatles', 1968);
+addToCollection('Late Registration', 'Kanye West', 2005)
+
+console.log(collection);
